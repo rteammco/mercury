1346af2fa0b7a8bc5be6206bd8ec5a0ba64399da
Richard Teammco <teammcr192@gmail.com>
2016-11-12 16:12:11 -0600

    Added player node on screen. Also a few more style fixes.

--- Mercury/GameScene.swift
+++ Mercury/GameScene.swift
@@ -10,15 +10,34 @@ import SpriteKit
 import GameplayKit
 
 class GameScene: SKScene {
-
+  
+  // The player node.
+  private var playerNode : SKShapeNode?
+  
+  // Template for the node (animated).
+  // TODO: remove this, and its initialization in didMove()
   private var spinnyNode : SKShapeNode?
-
-  // Called whenever the scene is presented into the view
+  
+  // Called whenever the scene is presented into the view.
   override func didMove(to view: SKView) {
+    // Add the player to the bottom of the screen
+    self.playerNode = SKShapeNode.init(rectOf: CGSize.init(width: 140, height: 140))
+    if let playerNode = self.playerNode {
+      // Screen is centered at (0, 0)
+      let xPos = 0
+      let yPos = -Int(self.size.height / 2) + 150
+      playerNode.position = CGPoint(x: xPos, y: yPos)
+      playerNode.fillColor = SKColor.green
+      playerNode.lineWidth = 2.5
+      playerNode.strokeColor = SKColor.red
+      self.addChild(playerNode)
+    }
+    
     // Create shape node to use during mouse interaction
+    // TODO: remove this
     let w = (self.size.width + self.size.height) * 0.05
+    print(w)
     self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
-
     if let spinnyNode = self.spinnyNode {
       spinnyNode.lineWidth = 2.5
       spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(M_PI), duration: 1)))
@@ -27,7 +46,7 @@ class GameScene: SKScene {
                                         SKAction.removeFromParent()]))
       }
   }
-
+  
   func touchDown(atPoint pos : CGPoint) {
     if let n = self.spinnyNode?.copy() as! SKShapeNode? {
       n.position = pos
@@ -35,15 +54,15 @@ class GameScene: SKScene {
       self.addChild(n)
     }
   }
-
+  
   func touchMoved(toPoint pos : CGPoint) {
     if let n = self.spinnyNode?.copy() as! SKShapeNode? {
       n.position = pos
       n.strokeColor = SKColor.blue
       self.addChild(n)
-      }
+    }
   }
-
+  
   func touchUp(atPoint pos : CGPoint) {
     if let n = self.spinnyNode?.copy() as! SKShapeNode? {
       n.position = pos
@@ -51,28 +70,29 @@ class GameScene: SKScene {
       self.addChild(n)
     }
   }
-
+  
+  // Called when user starts a touch action.
   override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print("touchesBegan")
     for t in touches { self.touchDown(atPoint: t.location(in: self)) }
   }
-
+  
+  // Called when user moves (drags) during a touch action.
   override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print("touchesMoved")
     for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
   }
-
+  
+  // Called when user finishes a touch action.
   override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print("touchesEnded")
     for t in touches { self.touchUp(atPoint: t.location(in: self)) }
   }
-
+  
+  // Called when a touch action is interrupted or otherwise cancelled.
   override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-    print("touchesCanceled")
     for t in touches { self.touchUp(atPoint: t.location(in: self)) }
   }
-
+  
   override func update(_ currentTime: TimeInterval) {
     // Called before each frame is rendered
   }
+  
 }
--- Mercury/GameViewController.swift
+++ Mercury/GameViewController.swift
@@ -11,42 +11,39 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-
+  
   override func viewDidLoad() {
     super.viewDidLoad()
-
     if let view = self.view as! SKView? {
       // Load the SKScene from 'GameScene.sks'
       if let scene = SKScene(fileNamed: "GameScene") {
         // Set the scale mode to scale to fit the window
         scene.scaleMode = .aspectFill
-
         // Present the scene
         view.presentScene(scene)
       }
-
       view.ignoresSiblingOrder = true
-
       view.showsFPS = true
       view.showsNodeCount = true
     }
   }
-
+  
   override var shouldAutorotate: Bool {
     return false
   }
-
+  
   override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
     // This game only plays in right-side-up portrait mode
     return .portrait
   }
-
+  
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Release any cached data, images, etc that aren't in use.
   }
-
+  
   override var prefersStatusBarHidden: Bool {
     return true
   }
+  
 }
