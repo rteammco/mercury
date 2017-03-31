21d0c3b08c3091ca4730bd486a2dbbef51ddda4e
Richard Teammco <teammcr192@gmail.com>
2017-03-31 10:44:46 -0500

    Text nodes now get correctly removed from game.
    Before they faded out but they were never removed.

--- Mercury/GameScene.swift
+++ Mercury/GameScene.swift
@@ -86,10 +86,15 @@ class GameScene: SKScene, EventCaller, GameStateListener {
     labelNode.position = CGPoint(x: self.frame.midX, y: self.frame.midY)
     addChild(labelNode)
     
-    let waitAction = SKAction.wait(forDuration: textOnScreenDuration)
-    let fadeAction = SKAction.fadeOut(withDuration: textFadeOutDuration)
-    let textFadeOut = SKAction.run({() in labelNode.run(fadeAction)})
-    run(SKAction.sequence([waitAction, textFadeOut]))
+    let waitBeforeFade = SKAction.wait(forDuration: textOnScreenDuration)
+    let textFadeOut = SKAction.run({
+      () in labelNode.run(SKAction.fadeOut(withDuration: textFadeOutDuration))
+    })
+    let waitBeforeRemove = SKAction.wait(forDuration: textFadeOutDuration)
+    let removeTextNode = SKAction.run({
+      () in labelNode.run(SKAction.removeFromParent())
+    })
+    run(SKAction.sequence([waitBeforeFade, textFadeOut, waitBeforeRemove, removeTextNode]))
   }
   
   // Returns a scaled version of the given normalized position. Position (0, 0) is in the center. If the X coordinate is -1, that's the left-most side of the screen; +1 is the right-most side. Since the screen is taller than it is wide, +/-1 in the Y axis is not going to be completely at the bottom or top.
