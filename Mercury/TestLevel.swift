6fbb67f66b4f358d563622d7c90fb9e501aa50d0
Richard Teammco <teammcr192@gmail.com>
2017-04-01 14:32:24 -0500

    Cleaned up the test level code.

--- Mercury/TestLevel.swift
+++ Mercury/TestLevel.swift
@@ -11,23 +11,17 @@ import SpriteKit
 class TestLevel: GameScene {
     
   override func initializeScene() {
+    subscribeToStateChanges()
     createPlayer(atPosition: CGPoint(x: 0.0, y: -1.0))
-    
-    // Need to call this to handle any state changes that are triggered by other objects.
-    self.subscribeToStateChanges()
-    
-    //when(TimerEvent(seconds: 5)).execute(action: DisplayText(message: "Hello, World"))
-    //when(TimerEvent(seconds: 1)).execute(action: SpawnEnemy("test")).until(NumberOfEnemiesSpawned(equals: 10))
     
     let phase1 = createEventPhase()
     phase1.execute(action: SpawnEnemy("test"))
-    phase1.when(EnemyDies()).execute(actions: SpawnEnemy("test"), DisplayText("Enemy Died!")).until(NumberOfEnemiesSpawned(equals: 3)).then(DisplayText("You Did It!!"))
+    phase1.when(EnemyDies()).execute(action: SpawnEnemy("test")).until(NumberOfEnemiesSpawned(equals: 3)).then(DisplayText("You Did It!!"))
     
     let phase2 = createEventPhase()
     phase2.when(TimerFires(afterSeconds: 3)).execute(action: DisplayText("PHASE 2")).then(when: TimerFires(afterSeconds: 3)).execute(action: DisplayText("DONE"))
     
     setPhaseSequence(phase1, phase2)
-    
     start(withCountdown: 5)
   }
   
