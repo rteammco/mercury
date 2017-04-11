e15bd2ff554558a6f26b2f01ab5995d7879afffe
Richard Teammco <teammcr192@gmail.com>
2017-04-11 09:15:09 -0500

    Updated ParticleSystems a little, but no explosions.
    Explosions don't want to work, because can't add emitters directly to the GameScene for some reason.

--- Mercury/ParticleSystems.swift
+++ Mercury/ParticleSystems.swift
@@ -12,70 +12,42 @@ import SpriteKit
 struct ParticleSystems {
   
   // Runs the given particle effects emitter on the given object. The offset position will be the location on the object's node where the effect will be positioned. The given duration will be how long the effect will last.
-  static func runEffect(_ emitter: SKEmitterNode, on gameObject: GameObject, atOffset relativePosition: CGPoint, forDuration duration: TimeInterval) {
+  static func runEffect(_ emitter: SKEmitterNode, on parentNode: SKNode, atOffset relativePosition: CGPoint, forDuration duration: TimeInterval) {
     let effectNode = SKEffectNode()
     effectNode.position = relativePosition
     effectNode.addChild(emitter)
     effectNode.zPosition = 0
     let addEffect = SKAction.run({
-      () in gameObject.gameSceneNode?.addChild(effectNode)
+      () in parentNode.addChild(effectNode)
     })
     let wait = SKAction.wait(forDuration: duration)
     let removeEffect = SKAction.run({
       () in effectNode.removeFromParent()
     })
-    gameObject.gameSceneNode?.run(SKAction.sequence([addEffect, wait, removeEffect]))
+    parentNode.run(SKAction.sequence([addEffect, wait, removeEffect]))
   }
   
   // Runs a specific bullet impact effect that will create a small "spark" effect at the bullet's impact location. This spark effect will be oriented in the reverse orientation of the bullet's trajectory.
   static func runBulletImpactEffect(bullet: Bullet, target: GameObject) {
-    if let emitter = SKEmitterNode(fileNamed: "ImpactSpark.sks") {
+    if let emitter = SKEmitterNode(fileNamed: "ImpactSpark.sks"), let parentNode = target.gameSceneNode {
       if let bulletNode = bullet.gameSceneNode {
         emitter.emissionAngle = bulletNode.zRotation + CGFloat.pi
       }
       let bulletPosition = bullet.getPosition()
       let targetPosition = target.getPosition()
       let positionOffset = CGPoint(x: bulletPosition.x - targetPosition.x, y: bulletPosition.y - targetPosition.y)
-      ParticleSystems.runEffect(emitter, on: target, atOffset: positionOffset, forDuration: 0.1)
+      runEffect(emitter, on: parentNode, atOffset: positionOffset, forDuration: 0.1)
     }
   }
   
   static func runExplosionEffect(on gameObject: GameObject) {
-    
-//    // TODO REMOVE AND FIX
-//    let node = SKShapeNode(rectOf: CGSize.init(width: 50, height: 50))
-//    node.position = CGPoint(x: 0, y: 0)
-//    //node.alpha = 0.0
-//    if let emitter = SKEmitterNode(fileNamed: "ImpactExplosion.sks") {
-//      emitter.position = CGPoint(x: 0, y: 0)
-//      emitter.name = "explosion"
-//      //emitter.alpha = 1.0
-//      //emitter.isHidden = false
-//      //emitter.targetNode = self
-//      node.addChild(emitter)
-//    }
-//    addChild(node)
-//    if let emitter = SKEmitterNode(fileNamed: "ImpactExplosion.sks") {
-//      if let scene = gameObject.gameSceneNode?.scene {
-//      emitter.position = gameObject.getPosition()
-//      emitter.name = "explosion"
-//      emitter.targetNode = scene
-//      emitter.zPosition = 2
-//      //let effectNode = SKEffectNode()
-//      //effectNode.addChild(emitter)
-//      //effectNode.zPosition = 0
-//      scene.addChild(emitter)
-//      /*
-//       emitter.position = CGPointMake(posX,posy);
-//       emitter.name = @"explosion";
-//       emitter.targetNode = self.scene;
-//       emitter.numParticlesToEmit = 1000;
-//       emitter.zPosition=2.0;
-// */
-//        print("NODE ADDED")
-//        
-//      }
-      //ParticleSystems.runEffect(emitter, on: gameObject, atOffset: CGPoint(x: 0, y: 0), forDuration: 0.1)
+    if let emitter = SKEmitterNode(fileNamed: "ImpactExplosion.sks") {
+      let effectNode = SKEffectNode()
+      effectNode.position = gameObject.getPosition()
+      effectNode.addChild(emitter)
+      effectNode.zPosition = 0
+      effectNode.name = "explosion"
+      // TODO: gameObject.gameState.inform(.createEffectsNode, node)
     }
   }
   
