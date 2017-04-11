78d16b9907f7686a3a50f3b5682a1bee5622dcfa
Richard Teammco <teammcr192@gmail.com>
2017-04-11 11:24:54 -0500

    Cleaned up the ParticleSystems code a little bit.

--- Mercury/ParticleSystems.swift
+++ Mercury/ParticleSystems.swift
@@ -11,17 +11,24 @@ import SpriteKit
 
 struct ParticleSystems {
   
-  // Runs the given particle effects emitter on the given object. The offset position will be the location on the object's node where the effect will be positioned. The given duration will be how long the effect will last.
-  private static func runEffect(_ emitter: SKEmitterNode, on parentNode: SKNode, atOffset relativePosition: CGPoint, forDuration duration: TimeInterval) {
-    emitter.position = relativePosition
-    let addEffect = SKAction.run({
-      () in parentNode.addChild(emitter)
-    })
+  // Starts running the given effect for the given duration. After the duration runs out, the effect will automatically remove itself from its parent node or scene.
+  // A negative duration is invalid and will do nothing (i.e. the effect will run forever).
+  private static func runEffect(_ emitter: SKEmitterNode, forDuration duration: TimeInterval) {
+    if duration < 0 {
+      return
+    }
     let wait = SKAction.wait(forDuration: duration)
     let removeEffect = SKAction.run({
       () in emitter.removeFromParent()
     })
-    parentNode.run(SKAction.sequence([addEffect, wait, removeEffect]))
+    emitter.run(SKAction.sequence([wait, removeEffect]))
+  }
+  
+  // Runs the given particle effects emitter on the given object. The offset position will be the location on the object's node where the effect will be positioned. The given duration will be how long the effect will last.
+  private static func runEffect(_ emitter: SKEmitterNode, on parentNode: SKNode, atOffset relativePosition: CGPoint, forDuration duration: TimeInterval) {
+    emitter.position = relativePosition
+    parentNode.addChild(emitter)
+    runEffect(emitter, forDuration: duration)
   }
   
   // Runs a specific bullet impact effect that will create a small "spark" effect at the bullet's impact location. This spark effect will be oriented in the reverse orientation of the bullet's trajectory.
@@ -37,15 +44,12 @@ struct ParticleSystems {
     }
   }
   
+  // Runs an explosion effect at the given object's location. This effect is not tied to the given object, but rather simulates it exploading in a separate animation.
   static func runExplosionEffect(on gameObject: GameObject) {
     if let emitter = SKEmitterNode(fileNamed: "ImpactExplosion.sks") {
       emitter.position = gameObject.getPosition()
       emitter.numParticlesToEmit = 1000
-      let wait = SKAction.wait(forDuration: 2)
-      let removeEffect = SKAction.run({
-        () in emitter.removeFromParent()
-      })
-      emitter.run(SKAction.sequence([wait, removeEffect]))
+      runEffect(emitter, forDuration: 2.0)
       gameObject.gameState.inform(.createParticleEffect, value: emitter)
     }
   }
